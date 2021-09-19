300a4f22e642f50e154a9980358bb28fb6b27d2c
Istallia <55042065+istallia@users.noreply.github.com>
2021-09-19 09:31:05 +0900

    [Add] 複数ファイル対応のファイル読み出しを実装

--- listup_tool.py
+++ listup_tool.py
@@ -22,3 +22,20 @@ import listup_tool_lib as tool
 
 #--- タイトルを描画
 print('------ 「ニコニコ素材リストアップツール」v0.7.0 by @is_ptcm ------\n')
+
+
+#--- 読み出すファイルをコマンドライン引数から取得(複数可)
+file_list = []
+if len(sys.argv) < 2:
+	print('ファイル名(or パス)を入力してください: ', end='')
+	file_list.append(input().strip())
+else:
+	for i in range(1,len(sys.argv)):
+		file_list.append(sys.argv[i])
+for i in range(len(file_list)):
+	file_list[i] = os.path.abspath(file_list[i])
+file_list = list(set(file_list))
+
+
+#--- デバッグ用
+input()
