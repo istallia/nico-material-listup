9d5c855ea17ab964cc38f6329c9ff321b6b55236
Istallia <55042065+istallia@users.noreply.github.com>
2021-09-19 10:07:13 +0900

    [Add] 無視する拡張子のリストを外部から設定できるように

--- listup_tool.py
+++ listup_tool.py
@@ -24,6 +24,16 @@ import listup_tool_lib as tool
 print('------ 「ニコニコ素材リストアップツール」v0.7.0 by @is_ptcm ------\n')
 
 
+#--- 除外リストを作成
+exclude_ext_list = ['lwi', 'avi', 'mp4', 'flv', 'mov', 'asf', 'mkv', 'webm', 'mpg', 'm2ts', 'mpg', 'mpeg', 'wav', 'mp3', 'ogg', 'wma', 'm4a', 'flac', 'aif', 'aiff', 'aac', 'mid', 'midi', 'jpg', 'jpeg', 'png', 'gif', 'bmp', 'webp', 'ico', 'zip', 'lzh', '7z', 'gz', 'tar', 'exe', 'dll']
+if os.path.isfile('./exclude-ext.txt'):
+	# 追加除外リストが存在すれば読み込む
+	content = ''
+	with open('./exclude-ext.txt', mode='r', encoding='utf-8') as f:
+		content = f.read()
+	exclude_ext_list.extend(re.findall(r'[-\w]+', content))
+
+
 #--- 読み出すファイル/フォルダをコマンドライン引数から取得(複数可)
 input_list = []
 if len(sys.argv) < 2:
@@ -34,7 +44,10 @@ else:
 	for i in range(1,len(sys.argv)):
 		input_list.append(sys.argv[i])
 for i in range(len(input_list)):
-	input_list[i] = os.path.abspath(input_list[i])
+	root, ext = os.path.splitext(input_list[i])
+	if not ext[1:] in exclude_ext_list:
+		input_list[i] = os.path.abspath(input_list[i])
+		print(input_list[i])
 input_list = list(set(input_list))
 
 
@@ -47,10 +60,10 @@ for i in range(len(input_list)):
 	elif os.path.isfile(input_list[i]):
 		file_list.append(input_list[i])
 for i in range(len(dir_list)):
-	files = glob.glob(dir_list + '\\**')
+	files = glob.glob(dir_list[i] + '\\**')
 	for j in range(len(files)):
 		file_list.append(os.path.abspath(files[j]))
 
 
 #--- デバッグ用
-input()
+input('Enterで終了:')
